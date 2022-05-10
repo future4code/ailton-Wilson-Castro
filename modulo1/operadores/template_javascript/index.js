6cc528070be1b77ede42e107f22624eb36fcdf15
WilsonCastro32 <wilson.fcastro@hotmail.com>
2022-05-10 18:00:32 -0400

    Entrega 10/05/22

--- modulo1/operadores/template_javascript/index.js
+++ modulo1/operadores/template_javascript/index.js
@@ -58,11 +58,121 @@ console.log(resultado);*/
 
 // Exercício 3
 
-let idade = prompt("Quantos anos você tem?");
+/*let idade = prompt("Quantos anos você tem?");
 
 console.log("Sua idade em meses é:", idade*12, "meses");
 console.log("Sua idade em dias é:", idade*12*30, "dias");
-console.log("Sua idade em horas é:", idade*12*30*24, "horas");
+console.log("Sua idade em horas é:", idade*12*30*24, "horas");*/
+
+// Exercício 4
+
+/*let num1 = Number(prompt("Digite um número:"));
+let num2 = Number(prompt("Digite outro número:"));
+let result1 = num1 > num2;
+let result2 = num1 === num2;
+let result3 = num1 % num2;
+let divisao1 = result3 === 0;
+let result4 = num2 % num1;
+let divisao2 = result4 === 0;
+
+
+console.log("o primeiro número é maior que o segundo?", result1);
+console.log("o primeiro número é igual ao segundo?", result2);
+console.log("o primeiro número é divisível pelo segundo?", divisao1);
+console.log("o segundo número é divisível pelo primeiro?", divisao2);*/
+
+
+//DESAFIO
+
+//Exercício 1
+
+//Letra A)
+
+/*let tempF1 = 77;
+let tempK1 = (tempF1 - 32) * (5/9) + 273.15;
+console.log("A temperatura de 77 ॰F em Kelvin é:", tempK1,"K");
+
+//Letra B)
+let tempC1 = 80;
+let tempF2 = tempC1 * (9/5) + 32;
+console.log("A temperatura de 80॰C em F é:", tempF2,"॰F");
+
+//Letra C)
+let tempC2 = 30;
+let tempF3 = tempC2 * (9/5) + 32;
+let tempK2 = (tempF3 - 32) * (5/9) + 273.15;
+console.log("A temperatura de 30॰C em F é:", tempF3,"॰F");
+console.log("A temperatura de 30॰C em K é:", tempK2,"K");
+
+//Letra D
+let tempC3 = Number(prompt("Digite uma temperatura em ॰C:"));
+let tempF4 = tempC3 * (9/5) + 32;
+let tempK3 = (tempF4 - 32) * (5/9) + 273.15;
+console.log("A temperatura digitada em F é:", tempF4,"॰F");
+console.log("A temperatura digitada em K é:", tempK3,"K");*/
+
+//Exercício 2
+
+//Letra A)
+
+/*let kWatt = 280;
+let valorHora = 0.05;
+
+console.log("R$", kWatt * valorHora);*/
+
+//Letra B)
+
+/*let desconto = 0.15;
+let kWatt = 1000;
+let valorHora = 0.05;
+let resultado = (kWatt * valorHora) - (kWatt * valorHora)*desconto
+console.log("R$", resultado);*/
+
+//Exercício 3
+
+//Letra A)
+
+/*let libra = 20;
+let kilo = libra / 2.2046;
+console.log("20 libras equivale a", kilo, "Kg");*/
+
+//Letra B)
+
+/*let onca = 10.5;
+let kilo = onca * 0.0283;
+console.log("10.5 onças equivale a", kilo, "Kg");*/
+
+//Letra C)
+
+/*let milha = 100;
+let metro = milha * 1609.34;
+console.log("100 milhas equivale a", metro, "m");*/
+
+//Letra D)
+
+/*let pes = 50;
+let metro = pes * 0.3048
+console.log("50 pés equivale a", metro, "m");*/
+
+//Letra E)
+
+/*let galao = 103.56;
+let litro = galao * 3.7854;
+console.log("103.56 galões equivale a", litro, "l");*/
+
+//Letra F)
+/*let xicara = 450;
+let litro = xicara * 0.284131;
+console.log("450 xícaras imperial equivale a", litro, "l");*/
+
+//Letra G)
+
+/*let libra = prompt("Digite um valor em libras");
+let kilo = libra / 2.2046;
+console.log(libra, "libra(s) equivale a", kilo, "Kg");*/
+
+
+
 
 
 
