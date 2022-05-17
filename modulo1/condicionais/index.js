472c03ae28b53a62a49ecc8a55f4df1d79f927ad
WilsonCastro32 <wilson.fcastro@hotmail.com>
2022-05-17 19:40:10 -0400

    Atualização Exercício 17 de Maio

--- modulo1/condicionais/index.js
+++ modulo1/condicionais/index.js
@@ -91,3 +91,157 @@ if (generoM === "FANTASIA" && preco <= 15) {
 
 
 //**********DEAFIO**********
+//Exercício 1
+
+/*let genero = prompt("Qual o gênero de filme que vão assistir?");
+let preco = prompt("Qual o preço do filme que vão assistir?");
+let lanche = prompt("Qual snack você irá comprar?")
+
+let generoM = genero.toUpperCase().trim();
+let precoM = preco.toUpperCase().trim();
+
+if (generoM === "FANTASIA" && preco <= 15) {
+    console.log("Bom filme!")
+    console.log(`Aproveite o(a) seu(sua) ${lanche}`)
+} else {
+  console.log("Escolha outro filme!")
+}*/
+
+//Exercício 2
+
+const ingressosNac = [1320,660,1980,880,440,1320,550,330,880,220,170,330]
+const ingressosInt = [1320 * 4.10, 660 * 4.10, 1980 * 4.10, 880 * 4.10, 440 * 4.10, 1320 * 4.10, 550 * 4.10, 330 * 4.10, 880 * 4.10, 220 * 4.10, 170 * 4.10, 330 * 4.10]
+
+let nome = prompt("Qual seu nome completo?");7
+let etapa = prompt("Digite SF se quer assistir a semi-final ou DT para assistir decisão do 3º lugar ou FI para assistir ao jogo final")
+let regiao = prompt("Digite IN para jogo internacional ou DO para jogo doméstico");
+let cat = Number(prompt("Escolha uma dessas categorias: 1, 2, 3 ou 4"))
+let qty = Number(prompt("Quantos ingressos quer comprar? (Digite somente números)"))
+
+  console.log(`Nome do cliente: ${nome}`)
+
+  switch(etapa){
+    case "SF":
+    console.log(`Etapa do jogo: Semi-Final`)
+    break
+
+    case "DT":
+    console.log(`Etapa do jogo: Disputa 3º Lugar`)
+    break
+
+    case "FI":
+    console.log(`Etapa do jogo: Final`)
+  }
+
+  switch(regiao){
+    case "IN":
+    console.log(`Tipo de jogo: Internacional`)
+    break;
+
+    case "DO":
+    console.log(`Tipo de jogo: Nacional`)
+  }
+
+  console.log(`Categoria: ${cat}`)
+
+  console.log(`Quantidade de ingressos: ${qty} ingresso(s)`)
+
+  console.log("----Valores---")
+
+  if(regiao === "DO" && etapa === "SF" && cat === 1){
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[0]}
+    Valor Total: ${ingressosNac[0] * qty}`)
+  } else if(regiao === "DO" && etapa === "DT" && cat === 1) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[1]}
+     Valor Total: ${ingressosNac[1] * qty}`)
+  } else if(regiao === "DO" && etapa === "FI" && cat === 1) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[2]}
+     Valor Total: ${ingressosNac[2] * qty}`)
+  } else if(regiao === "DO" && etapa === "SF" && cat === 2) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[3]}
+     Valor Total: ${ingressosNac[3] * qty}`)
+  } else if(regiao === "DO" && etapa === "DT" && cat === 2) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[4]}
+     Valor Total: ${ingressosNac[4] * qty}`)
+  } else if(regiao === "DO" && etapa === "FI" && cat === 2) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[5]}
+     Valor Total: ${ingressosNac[5] * qty}`)
+  } else if(regiao === "DO" && etapa === "SF" && cat === 3) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[6]}
+     Valor Total: ${ingressosNac[6] * qty}`)
+  } else if(regiao === "DO" && etapa === "DT" && cat === 3) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[7]}
+     Valor Total: ${ingressosNac[7] * qty}`)
+  } else if(regiao === "DO" && etapa === "FI" && cat === 3) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[8]}
+     Valor Total: ${ingressosNac[8] * qty}`)
+  } else if(regiao === "DO" && etapa === "SF" && cat === 4) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[9]}
+     Valor Total: ${ingressosNac[9] * qty}`)
+  } else if(regiao === "DO" && etapa === "DT" && cat === 4) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[10]}
+     Valor Total: ${ingressosNac[10] * qty}`)
+  } else if(regiao === "DO" && etapa === "FI" && cat === 4) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosNac[11]}
+     Valor Total: ${ingressosNac[11] * qty}`)
+  } else if(regiao === "IN" && etapa === "SF" && cat === 1) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[0]}
+     Valor Total: ${ingressosInt[0] * qty}`)
+  } else if(regiao === "IN" && etapa === "DT" && cat === 1) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[1]}
+     Valor Total: ${ingressosInt[1] * qty}`)
+  } else if(regiao === "IN" && etapa === "FI" && cat === 1) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[2]}
+     Valor Total: ${ingressosInt[2] * qty}`)
+  } else if(regiao === "IN" && etapa === "SF" && cat === 2) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[3]}
+     Valor Total: ${ingressosInt[3] * qty}`)
+  } else if(regiao === "IN" && etapa === "DT" && cat === 2) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[4]}
+     Valor Total: ${ingressosInt[4] * qty}`)
+  } else if(regiao === "IN" && etapa === "FI" && cat === 2) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[5]}
+     Valor Total: ${ingressosInt[5] * qty}`)
+  } else if(regiao === "IN" && etapa === "SF" && cat === 3) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[6]}
+     Valor Total: ${ingressosInt[6] * qty}`)
+  } else if(regiao === "IN" && etapa === "DT" && cat === 3) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[7]}
+     Valor Total: ${ingressosInt[7] * qty}`)
+  } else if(regiao === "IN" && etapa === "FI" && cat === 3) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[8]}
+     Valor Total: ${ingressosInt[8] * qty}`)
+  } else if(regiao === "IN" && etapa === "SF" && cat === 4) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[9]}
+     Valor Total: ${ingressosInt[9] * qty}`)
+  } else if(regiao === "IN" && etapa === "DT" && cat === 4) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[10]}
+     Valor Total: ${ingressosInt[10] * qty}`)
+  } else if(regiao === "IN" && etapa === "FI" && cat === 4) {
+    console.log(
+    `Valor do ingresso: R$ ${ingressosInt[11]}
+     Valor Total: ${ingressosInt[11] * qty}`)
+  }
